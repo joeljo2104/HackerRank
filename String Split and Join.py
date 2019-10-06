597dca6594320d90d8bba959a2e7d2c409b9b700
NCM <neenuchacko@live.in>
2019-10-06 18:16:19 +0530

    added description and changed split and join

--- String Split and Join.py
+++ String Split and Join.py
@@ -1,5 +1,6 @@
+'''Write a python function to split and join a line with '_' inputed and return the joined line '''
 def split_and_join(line):
-    a = line.split(" ")
-    b = "-".join(a)
-    return  b
+    s = line.split(" ")
+    r= "-".join(s)
+    return (r)
 
